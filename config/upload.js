85355e323a54bb9a67b31c2c3885fde25e59f5e1
962976642@qq.com <zhangchao1995>
2016-09-08 15:47:55 +0800

    新增文件上传处理逻辑
    新添加了文件上传处理墨迹multer中间件以及multer的配置

---
+++ config/upload.js
@@ -0,0 +1,28 @@
+/*
+* @Author: tianxing
+* @Date:   2016-09-08 15:18:05
+* @Last Modified by:   tianxing
+* @Last Modified time: 2016-09-08 15:39:37
+*/
+
+// 文件上传配置文件
+var multer = require('multer');
+// 配置multer双穿文件配置
+var storage = multer.diskStorage({
+	// destination是文件上传的制定目录
+	destination: function(req,file,cb){
+		// 指定上传文件到马哥目录
+		cb(null, './public/images/userimg');
+	},
+	// filename是双穿文件之后的名称
+	filename: function(req,file,cb){
+		// 指定上传文件之后的名称还是原名称
+		cb(null,file.originalname);
+	},
+});
+var upload = multer({
+	// 文件名为原文件名
+	storage: storage
+});
+// 导出upload接口
+module.exports = upload;
